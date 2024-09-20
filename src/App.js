012fd3c2de28970414c62740760b52b33e5b69b6
Suwarin Photak <suwarin.ph@kkumail.com>
2024-09-20 13:18:27 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+
 
 // Import all pages
 // For public
@@ -13,11 +13,7 @@ import ContactUs from './pages/ContactUs';
 import Login from './pages/Login';
 import ForgotPassword from './pages/ForgotPassword';
 import Register from './pages/Register';
-import Register2 from './pages/Register2'; // Remove duplicate
-import Register3 from './pages/Register3';
-import Register4 from './pages/Register4';
 
-// For trainer
 import TrainerHome from './trainer/TrainerHome';
 import TrainerWorkspace from './trainer/TrainerWorkspace';
 import TrainerAvailability from './trainer/TrainerAvailability';
@@ -26,16 +22,9 @@ import TrainerProgram from './trainer/TrainerProgram';
 import TrainerContactus from './trainer/TrainerContactus';
 import TrainerLogin from './trainer/TrainerLogin';
 
-// For admin
-import AdminWorkspace from './admin/admin_workspace';
-import AdminProfile from './admin/admin_profile';
-import AdminConfirmPayment from './admin/admin_ConfirmPayment';
-import AdminMemberM from './admin/admin_MemberM';
-import AdminProgramM from './admin/admin_ProgramM';
-import AdminTrainerM from './admin/admin_TrainerM';
-import AdminHome from './admin/admin_Home';
-import AddTrainingProgram from './admin/AddTrainingProgram';
-import EditTrainingProgram from './admin/EditTrainingProgram';
+import Register2 from './pages/Register2'; // Add this import
+import Register3 from './pages/Register3'; 
+import Register4 from './pages/Register4'; 
 
 function App() {
   // กำหนดบทบาทที่ต้องการทดสอบ ใช้ '' ถ้าต้องการดูหน้าสาธารณะ(public)
@@ -57,31 +46,18 @@ function App() {
           <Route path="/register3" element={<Register3 />} />
           <Route path="/register4" element={<Register4 />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-
-          {/* Trainer Pages */}
-          <Route path="/trainer-home" element={<TrainerHome />} />
+          {/*************/}
+          <Route path="/trainer-home" element={<TrainerHome />} /> {/* กำหนด route สำหรับ TrainerHome */}
           <Route path="/trainer-workspace" element={<TrainerWorkspace />} />
           <Route path="/trainer-schedule" element={<TrainerSchedule />} />
           <Route path="/trainer-availability" element={<TrainerAvailability />} />
           <Route path="/trainer-program" element={<TrainerProgram />} />
           <Route path="/trainer-contact-us" element={<TrainerContactus />} />
           <Route path="/trainer-login" element={<TrainerLogin />} />
-
-          {/* Admin Pages */}
-          <Route path="/admin-workspace" element={<AdminWorkspace />} />
-          <Route path="/admin-profile" element={<AdminProfile />} />
-          <Route path="/admin-confirm-payment" element={<AdminConfirmPayment />} />
-          <Route path="/admin-member-management" element={<AdminMemberM />} />
-          <Route path="/admin-training-program-management" element={<AdminProgramM />} />
-          <Route path="/admin-trainer-management" element={<AdminTrainerM />} />
-          <Route path="/admin-home" element={<AdminHome />} />
-          <Route path="/admin-training-edit" element={<EditTrainingProgram />} />
-          <Route path="/admin-training-add" element={<AddTrainingProgram />} />
-
           {/* Optional: Add a route for 404 */}
           <Route path="*" element={<Home />} /> {/* Redirect to Home or a 404 component */}
         </Routes>
-        <Footer />
+        
       </div>
     </Router>
   );
